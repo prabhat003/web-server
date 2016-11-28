d76a4f72b65dc526c245a29a4be13a47d9a9c329
Prabhat Kumar Prabhakar <prabhtp@winjit.com>
2016-11-28 15:16:10 +0530

    initaialize repo

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+var middleware ={
+	requireAuthentication: function(req, res, next){
+		console.log('Private route hit');
+		next();
+	},
+	logger: function(req, res, next){
+		var date = new Date().toString();
+		console.log(req.method + ' ' + req.originalUrl + '  ' + date);
+		next();
+	}
+};
+//app.use(middleware.requireAuthenticationl);
+app.use(middleware.logger);
+
+// app.get('/', function(req, res){
+// 	res.send('Hello Express');
+// });
+
+app.get('/about',middleware.requireAuthentication, function(req, res){
+	res.send('About Us');
+});
+
+
+app.use(express.static(__dirname + '/public'));
+// console.log(__dirname);
+
+app.listen(PORT, function(){
+	console.log('Server listening on port no ' + PORT);
+});
